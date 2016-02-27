7a03525f95568c440611198f770fd29f88c214ed
RamaKrushnuduVadela <ramakrushnudu@gmail.com>
2016-02-27 18:13:15 +0530

    modified the changes

--- public/src/tdesk-location.js
+++ public/src/tdesk-location.js
@@ -1,12 +1,18 @@
- var bounds = new google.maps.LatLngBounds();
-   
+var bounds = new google.maps.LatLngBounds();
+
 $(document).ready(function () {
-$("#btnPush").click(function(e){
-    e.preventDefault();
-    PushDataToFireBase();
-}
-);
- 
+    tdesk.config.user(tdesk.readQuery("id"), function (userConfig) {
+        //console.log(value);
+        window.userConfig = userConfig;
+        // init(userConfig.track.lat, userConfig.track.lng);
+    });
+
+    $("#btnPush").click(function (e) {
+        e.preventDefault();
+        PushDataToFireBase();
+    }
+        );
+
     navigator.geolocation.getCurrentPosition(function (position) {
         var mapElement = document.getElementById('mapPlaceholder');
 
@@ -21,9 +27,24 @@ $("#btnPush").click(function(e){
         AssignValuesToFields(position.coords.latitude, position.coords.longitude);
     });
 
-//SetBaseLocation();
-
+    //SetBaseLocation();
+    //  GetWatchFunction();
 });
+function GetWatchFunction() {
+    var options = {
+        enableHighAccuracy: false,
+        timeout: 50000,
+        maximumAge: 0
+    };
+
+var watchID = navigator.geolocation.watchPosition(function (position) {
+    PushLiveData(position.coords.latitude, position.coords.longitude, watchID);
+   },options);
+}
+function PushLiveData(latitude, longitude, watchId) {
+    alert(latitude + "," + longitude + "," + watchId);
+
+}
 
 function markCurrentLocation(map, latitude, longitude) {
 
@@ -37,37 +58,37 @@ function markCurrentLocation(map, latitude, longitude) {
 
 }
 
-function AssignValuesToFields(latitude,longitude)
-{
-$("#driverLatitude").val(latitude);
-$("#driverLongitude").val(longitude);
+function AssignValuesToFields(latitude, longitude) {
+    $("#driverLatitude").val(latitude);
+    $("#driverLongitude").val(longitude);
 
 }
 
-function PushDataToFireBase()
-{
+function PushDataToFireBase() {
 
-    var latitude=$("#driverLatitude").val();
-    var longitude=$("#driverLongitude").val();
-    alert(latitude+","+longitude);
-    
+    var latitude = parseFloat($("#driverLatitude").val());
+    var longitude = parseFloat($("#driverLongitude").val());
+    var latlang = { lat: latitude, lng: longitude };
+    var vechicle = new tdesk.Vehicle(userConfig.track.loc, userConfig.userid);
+    vechicle.position(latlang);
+    alert(latitude + "," + longitude);
+
 }
 
-function SetBaseLocation()
-{
-  //var myLatLng = {lat: 17.444751714467815, lng: 78.36154093371575};
-     var myLatLng=  new google.maps.LatLng(17.444751714467815, 78.36154093371575);
- bounds.extend(myLatLng);
+function SetBaseLocation() {
+    //var myLatLng = {lat: 17.444751714467815, lng: 78.36154093371575};
+    var myLatLng = new google.maps.LatLng(17.444751714467815, 78.36154093371575);
+    // bounds.extend(myLatLng);
      
-        var map = new google.maps.Map(document.getElementById('mapPlaceholder'), {
-          zoom: 4,
-          center: myLatLng
-        });
+    var map = new google.maps.Map(document.getElementById('mapPlaceholder'), {
+        zoom: 4,
+        center: myLatLng
+    });
 
-        var marker = new google.maps.Marker({
-          position: myLatLng,
-          map: map,
-          title: 'Hello World!'
-        });
- map.fitBounds(bounds);
+    var marker = new google.maps.Marker({
+        position: myLatLng,
+        map: map,
+        title: 'Hello World!'
+    });
+    marker.setMap(map);
 }
