5fe0b751b4b07b70780e08e9e2d4e0a4b9c78012
Dhananjaya <dhananjayak@yahoo.com>
2016-02-27 13:36:43 +0530

    tdesk

---
+++ public/src/tdesk-config.js
@@ -0,0 +1,39 @@
+
+(function(global){
+	'use strict';
+
+	var FIREBASE_URL = "https://tdesk-config.firebaseio.com/tdesk-config";
+	var firebaseRef = new Firebase("https://tdesk-config.firebaseio.com/tdesk-config");
+
+	var config = {
+		set: function(value){
+			firebaseRef.set(value, function(error){
+				if (error){
+					console.log(error);
+					return;
+				}
+
+				console.log('synchronized...');
+			});
+		},
+		user:function(email, callback){
+			firebaseRef.once('value', function(dataSnapshot) {
+  			// handle read data.
+  				var users = dataSnapshot.val();
+
+  				var filtered =
+  					users.filter(function(user){
+  						console.log(user)
+  						return (user.email === email);
+  					});
+
+  				callback(filtered[0]);
+  			});
+		}
+	};
+
+	if (typeof global.tdesk === 'undefined')
+		global.tdesk = {};
+
+	global.tdesk.config = config;
+}(window));
---
+++ public/src/tdesk-vehicle.js
@@ -0,0 +1,22 @@
+(function(global){
+	'use strict';
+
+	var FIREBASE_URL = "https://tdesk-track.firebaseio.com";
+	
+	function Vehicle(source, user){
+		this.user = user;
+		this.source = source;
+		this.firebaseRef = new Firebase(FIREBASE_URL + '/' + source + '/' + user + '/');
+	}
+
+	Vehicle.prototype.position = function(latLng) {
+		var sourceRef = this.firebaseRef.push();
+
+		sourceRef.set(latLng);
+	}
+
+	if (typeof global.tdesk === 'undefined')
+		global.tdesk = {};
+
+	global.tdesk.Vehicle = Vehicle;
+}(window));
