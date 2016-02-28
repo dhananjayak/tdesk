447feec0fba5b0821f97bc435124ad291adba42f
Dhananjaya <dhananjayak@yahoo.com>
2016-02-28 07:17:40 +0530

    locate vehicle

--- public/src/tdesk-locate-vehicle.js
+++ public/src/tdesk-locate-vehicle.js
@@ -3,28 +3,8 @@
             var vehiclePositions = {};
 
             window.userConfig = userConfig;
-
-            /*
-            var map = init(userConfig.track.lat, userConfig.track.lng, 'map_locations');
-
-            tdesk.vehicles.all(userConfig.track.loc, function(response){
-                Object.keys(response).forEach(function(user){
-                    var usersLatLng = response[user];
-                
-                    console.log('points changed..');
-
-                    if (vehiclePositions[user]){
-                        vehiclePositions[user].setPosition({lat:usersLatLng.lat, lng:usersLatLng.lng});
-                    }
-                    else{
-                        vehiclePositions[user] =
-                            PointVehicle(usersLatLng, map);    
-                    }
-            });
-            */
-        });
-
-            //initLocateVeh();
+   
+            initLocateVeh();
         });
 });
 
